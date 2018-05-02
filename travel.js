abfed4304f66f59ba861c5006b444692a6adf680
Laura Garrido Pérez <l.garrido.perez@gmail.com>
2018-05-02 16:08:33 +0200

    Añado todos los archivos de proyecto a develop

---
+++ travel.js
@@ -0,0 +1,27 @@
+function movingClock(){ 
+   		var exactTime = new Date()
+   		var hour = exactTime.getHours()
+   		var minute = exactTime.getMinutes()
+   		var second = exactTime.getSeconds()
+   		minute = checkTime (minute)
+   		second = checkTime (second)
+
+   		clock = hour + ":" + minute + ":" + second;
+
+   		document.form_GMT2.timeGMT2.value = clock;
+
+   		setTimeout("movingClock()",1000);
+	} 
+	function checkTime (i){
+		if(i<10){i="0"+i};
+		return i;
+	}
+
+
+// hay que incluir el button en html?
+var button = document.querySelector('button');
+
+button.onclick = function() {
+  var name = prompt('What is your name?');
+  alert('Hello ' + name + ', nice to see you!');
+}
