9eb6f5cb3ca36188e22c65506fa2c531100d3250
eleiiuarei <l.garrido.perez@gmail.com>
2018-05-24 21:59:58 +0200

    funciones change_picture y welcome_country funcionan ok; click_search solo con console

--- javascript/img-focus.js
+++ javascript/img-focus.js
@@ -1,11 +1,27 @@
 
 
-function welcome_country () {
-	var countrySpain = document.getElementById('countryPicture');
-	countrySpain.addEventListener('mouseenter', function () {countrySpain.src = '../img/Cuba.jpg'});
-};
-
-function dame_la_hora() {
+/*function dame_la_hora() {
 	var fecha = new Date();
 	document.getElementById('fecha').innerHTML = fecha;
-}
+};*/
+
+function change_picture () {
+	var countrySpain = document.getElementById('countryPicture');
+	countrySpain.addEventListener('mouseenter', function () {countrySpain.src = '../img/Spain2.jpg'});
+};
+
+function welcome_country () {
+	var welcomeSpain = document.getElementById('countryName');
+	welcomeSpain.addEventListener('mouseenter', function () {welcomeSpain.innerHTML = "Welcome to Spain!"});	
+	welcomeSpain.addEventListener('mouseleave', function () {welcomeSpain.innerHTML = "Already leaving?"});
+};
+
+function clear_search () {
+	var clickSearch = document.getElementById('clickS');
+	clickSearch.addEventListener('focus', function() {clickSearch.style.background = "yellow"});
+	clickSearch.addEventListener('blur', function() {clickSearch.style.background = "white"});
+};
+
+//, 
+
+//
