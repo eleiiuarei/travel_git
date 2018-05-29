31966196c391a49d39e6ee96b01fb4bdea6644f0
eleiiuarei <l.garrido.perez@gmail.com>
2018-05-29 19:17:38 +0200

    add var countries para acceder por class country_name. funciona solo con el último elemento de array. pte revisar como acceder a todos a la vez

--- javascript/img-focus.js
+++ javascript/img-focus.js
@@ -1,16 +1,20 @@
 
 
-/*function dame_la_hora() {
-	var fecha = new Date();
-	document.getElementById('fecha').innerHTML = fecha;
-};*/
+var fecha = document.getElementById('fecha').innerHTML = new Date()
+
+
 var countrySpain = document.getElementById('countryPicture');
 var welcomeSpain = document.getElementById('countryName');
 
 countrySpain.addEventListener('mouseenter', function () {countrySpain.src = '../img/Spain2.jpg'});
-countrySpain.addEventListener('mouseout', function () {countrySpain.src = "../img/Spain.jpg"});
+countrySpain.addEventListener('mouseout', function () {countrySpain.src = '../img/Spain.jpg'});
 welcomeSpain.addEventListener('mouseover', function () {welcomeSpain.innerHTML = "Welcome to Spain!"});	
 welcomeSpain.addEventListener('mouseout', function () {welcomeSpain.innerHTML = "Already leaving?"});
+
+var countries = document.getElementsByClassName ('country_name');
+countries [3].addEventListener ('mouseenter', function() {countries[3].innerHTML = 'WELCOME to America!'});
+countries [1, 2].addEventListener ('mouseenter', function() {countries[1, 2].innerHTML = 'WELCOME to Asia!'});
+countries [0,1,2,3].addEventListener ('mouseleave', function () {countries [0,1,2,3].innerHTML = 'BYE BYE!'});
 
 
 /* function clear_search () {
