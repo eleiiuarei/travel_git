d4bce6608e8af739fb84e9fb0e2d65d0ab64e021
eleiiuarei <l.garrido.perez@gmail.com>
2018-05-29 22:47:15 +0200

    add evento cambio del link del logo, add evento hidden/show de alerta, modificado css, cambios de color en clicksearch y class en div

--- javascript/img-focus.js
+++ javascript/img-focus.js
@@ -2,7 +2,6 @@
 
 var fecha = document.getElementById('fecha').innerHTML = new Date()
 
-
 var countrySpain = document.getElementById('countryPicture');
 var welcomeSpain = document.getElementById('countryName');
 
@@ -20,23 +19,48 @@ countries [0,1,2,3].addEventListener ('mouseleave', function () {countries [0,1,
 
 //cambios de color en motor de búsqueda
 var clickSearch = document.getElementById('clickS');
+
 clickSearch.addEventListener('focus', function() {clickSearch.style.background = "yellow"});
 clickSearch.addEventListener('blur', function() {clickSearch.style.background = "white"});
 
+//cambio del enlace principal
+var mainLogo = document.getElementById('mainLogo');
+var linkLogo = document.getElementById ('linkLogo');
+
+//no entiendo por qué no funciona, porque te redirige y no lo ves??
+//mainLogo.onclick () = function () {mainLogo.className = "mainlogo2"};
+
+//creo que no funciona porque href no es attr de mainLogo sino de mainLogo>a ??
+//mainLogo.addEventListener('click', function() {mainLogo.href = "https://elpais.com/elpais/2018/01/11/paco_nadal/1515698051_919154.html"});
+
+linkLogo.addEventListener ('click', function() {linkLogo.href = "https://es.wikipedia.org/wiki/Viaje"});
+linkLogo.addEventListener ('dblclick', function() {linkLogo.href = "https://htmlcolorcodes.com/es/"});
+
+
+//cambio de clase con el evento mousemove. funciona pero no entiendo el evento
+var continents = document.getElementById('continentsBlock');
+continents.addEventListener('mousemove', function(){continents.className = 'continentsbg'});
+
+//mensaje de alerta al click on button
+var btnHistory = document.getElementById('btnHistory');
+var hiddenEurope = document.getElementById('hiddenEurope');
+var alertYes = document.getElementById('alertYes');
+
+btnHistory.addEventListener('click', function(){hiddenEurope.className ='show_alert'});
+alertYes.addEventListener('click', function(){hiddenEurope.className = 'hidden_alert'});
+
+
+
 /*function clear_search () {
-        
-      empty: empty_input () {
-              if (clickSearch.innerHTML != ""){
-                      return clickSearch.innerHTML = "";
-              }
-      }
-        clickSearch.addEventListener('focus', function() {clickSearch.style.background = "yellow"});
-        clickSearch.addEventListener('blur', function() {clickSearch.style.background = "white"});
+    if (clickSearch.innerHTML != " "){
+        return clickSearch.innerHTML = "";
+    } else {
+    	return clickSearch.innerHTML = "HOOOOLAAA";
+    }
 };
 
-function empty_input () {
-       if (clickSearch.innerHTML != ""){
-               return clickSearch.innerHTML = "";
-               };
-}
-console.log(empty_input());*/
+clickSearch.addEventListener('blur', clear_search());*/
+
+
+
+//fecha.addEventListener('onclick')
